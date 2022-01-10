7aaf3444590ce64498684ee99e3015d94f1f7f54
hongnguyen1508 <hongnguyenarmy@gmail.com>
2022-01-10 12:10:14 +0700

    notice signin

--- WebApplication/WebApplication/Controllers/ClientController.cs
+++ WebApplication/WebApplication/Controllers/ClientController.cs
@@ -109,6 +109,18 @@ namespace WebApplication.Controllers
         }
 
 
+        public IActionResult themgiohang(string matk, string masach, string soluong)
+        {
+            StoreContext context = HttpContext.RequestServices.GetService(typeof(WebApplication.Models.StoreContext)) as StoreContext;
+            context.themvaogiohang(matk, masach, soluong);
+            return Redirect("/Home/Index");
+        }
+
+        public IActionResult updategiohang(string matk, string masach, string soluong)
+        {
+            StoreContext context = HttpContext.RequestServices.GetService(typeof(WebApplication.Models.StoreContext)) as StoreContext;
+            return Redirect("/Home/cart");
+        }
 
         public IActionResult Login()
         {
